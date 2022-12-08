61bb868a45377733eb76490925d6f28ae683cbe7
Khalid Najmeddine <knajm574@gmail.com>
2022-12-07 17:14:27 -0700

    feat: implemented Nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { Authmodule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 
 @Module({
-  imports: [ConfigModule, Authmodule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    Authmodule, 
+    UserModule, 
+    BookmarkModule, 
+    PrismaModule,
+  ],
 })
 export class AppModule {}
