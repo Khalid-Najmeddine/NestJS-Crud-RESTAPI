8e21c0b69835e0e05e14ff36e25a4c6321320e23
Khalid Najmeddine <knajm574@gmail.com>
2022-12-12 16:48:34 -0700

    chore: prisma testing database teardown logic

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,13 @@
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
+import { PrismaService } from "../src/prisma/prisma.service";
 import { AppModule } from "../src/app.module";
 
 
 describe("App End2End Testing", () => {
 
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,12 +18,35 @@ describe("App End2End Testing", () => {
     app.useGlobalPipes(new ValidationPipe({
       whitelist: true,
     }));
-    await app.init() 
+    await app.init(); 
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDB();
   });
 
   afterAll(() => {
     app.close()
-  })
+  });
 
-  it.todo("The App should pass this test")
+  describe("Authentication", () => {
+    describe("Signup", () => {
+      it.todo("User Should Signup")
+    });
+    describe("Signin", () => {});
+      it.todo("User Should Signin")
+  });
+
+  describe("User", () => {
+    describe("Get me", () => {});
+    describe("Edit user", () => {});
+  });
+
+  describe("Bookmarks", () => {
+    describe("Create Bookmarks", () => {});
+    describe("Get Bookmarks", () => {});
+    describe("Get Bookmark by id", () => {});
+    describe("Edit Bookmark", () => {});
+    describe("Delete Bookmark", () => {});
+  });
+
 });
